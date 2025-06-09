abacfdf8404bce60fe128921715d0867dc77edff
bor1s <boris.prigozin@gmail.com>
2025-06-09 19:27:32 +0200

    Update firbaseConfig.ts

--- util/firbaseConfig.ts
+++ util/firbaseConfig.ts
@@ -5,9 +5,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyBWUuxJzzGlar9_bO-TnWQSS0YF9oIAhok",
   authDomain: "instacargram-8e788.firebaseapp.com",
   projectId: "instacargram-8e788",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  storageBucket: "instacargram-8e788.firebasestorage.app",
+  messagingSenderId: "982533527254",
+  appId: "1:982533527254:web:bf8bbeccaf8b6e0e5c9b77"
 };
 
 const app = initializeApp(firebaseConfig);
